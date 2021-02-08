ab7d49e300f09ad17d29b3839c39403440949b3a
Ikshit <ikshit.nigam@gmail.com>
2021-02-08 11:01:26 +0000

    Create fbase_pers.js

---
+++ fbase_pers.js
@@ -0,0 +1,2 @@
+var headIkshit =document.querySelector('#main > div.main__content > div > div.home-page--contents-inner-wrapper > div.styled__HomepageHeader-sc-1alowy6-1.doPcrk > div.main-banner.main-banner--signed-out > div.main-banner--content.main-banner--content-signed-out > div.main-banner--greeting-wrapper > h1 > span');
+headIkshit.innerText = "Ikshit is here";
