23bce8cf22f23421c78a5b7645b1b688ca636060
Ikshit <ikshit.nigam@gmail.com>
2021-02-08 11:15:15 +0000

    Update fbase_pers.js

--- fbase_pers.js
+++ fbase_pers.js
@@ -1,2 +1,3 @@
 var headIkshit =document.querySelector('#main > div.main__content > div > div.home-page--contents-inner-wrapper > div.styled__HomepageHeader-sc-1alowy6-1.doPcrk > div.main-banner.main-banner--signed-out > div.main-banner--content.main-banner--content-signed-out > div.main-banner--greeting-wrapper > h1 > span');
 headIkshit.innerText = "Ikshit is here";
+$('#hero-block-sub-section > div > div.tr-Poster-content > div > div > div > div > div:nth-child(2) > div > b').text('Ikshit is here')
